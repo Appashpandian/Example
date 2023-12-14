e983ca146e382d95043c3b46de3d47349c6b6796
Appashpandian <appashpandian005@gmail.com>
2023-12-14 14:31:48 +0530

    added a code

--- src/example/User.java
+++ src/example/User.java
@@ -1,5 +1,47 @@
 package example;
 
 public class User {
+	
+	private String empMail;
+	private String empName;
+	private String empUsername;
+	private String empPassword;
+	private int empId;
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpMail() {
+		return empMail;
+	}
+	public void setEmpMail(String empMail) {
+		this.empMail = empMail;
+	}
+	
+	public String getEmpUsername() {
+		return empUsername;
+	}
+	public void setEmpUsername(String empUsername) {
+		this.empUsername = empUsername;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getEmpPassword() {
+		return empPassword;
+	}
+	public void setEmpPassword(String empPassword) {
+		this.empPassword = empPassword;
+	}
+	
+	
+	
+	
+	
 
 }
